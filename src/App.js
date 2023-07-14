267b543639a0d7374a766a1a275531f80c00711b
Saurabhpankaj <Saurabhpankaj786@gmail.com>
2023-07-14 16:40:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Todo from "./todoreact/todo";
+//import Temp from "./weatherapp/temp";
+
+const App = () => {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+};
+
+export default App;
